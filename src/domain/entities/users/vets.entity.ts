b02b0e02d3f45da972dd0782a80d2b02758d0f3a
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-09 21:45:32 -0300

    review vets.entity

--- src/domain/entities/users/vets.entity.ts
+++ src/domain/entities/users/vets.entity.ts
@@ -1,4 +1,10 @@
-export interface IVetsEntity {
+import { IUsersEntity } from "./users.entity";
+
+export interface IVetsEntity extends IUsersEntity {
     specialty: string,
     CRMV: string,
+    queryValue: number,
+    whatsappLink: string,
+    experience: string,
+    teleconsultation: boolean
 }
