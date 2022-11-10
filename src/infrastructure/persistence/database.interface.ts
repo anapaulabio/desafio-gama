29ba9dadf740cbbbc785a60e803974a3643f1223
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-09 21:52:06 -0300

    create database.interface

---
+++ src/infrastructure/persistence/database.interface.ts
@@ -0,0 +1,7 @@
+export interface IDatabase {
+    list(type: any, dataWhere?: any): any[],
+    create(type: any, data: any): any,
+    read(type: any, dataId: any): any,
+    update(type: any, data: any): any,
+    delete(type: any, dataId: any): any
+}
