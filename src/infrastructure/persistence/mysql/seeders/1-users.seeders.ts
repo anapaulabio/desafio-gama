d060b75a8e50bd69545e7df65545b7858e598111
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-15 16:04:53 -0300

    review bug  phone number

--- src/infrastructure/persistence/mysql/seeders/1-users.seeders.ts
+++ src/infrastructure/persistence/mysql/seeders/1-users.seeders.ts
@@ -9,7 +9,7 @@ export default {
                 "code": "35530000",
                 "email": "jose_antonio@outlook.com",
                 "password": "12345678",
-                "phoneNumber": 56030,
+                "phoneNumber": "74992856030",
                 "whatsappLink": "whatsapp/74992856030/link.com"
             },
             {
@@ -18,7 +18,7 @@ export default {
                 "code": "32676265",
                 "email": "mkolscheck@gmail.com",
                 "password": "12345678",
-                "phoneNumber": 119812,
+                "phoneNumber": "11981456322",
                 "whatsappLink": "whatsapp/11981456322/link.com"
             },
             {
@@ -27,7 +27,7 @@ export default {
                 "code": "01001000",
                 "email": "oliveira_angelo@gmail.com",
                 "password": "12345678",
-                "phoneNumber": 8999865,
+                "phoneNumber": "89998654777",
                 "whatsappLink": "whatsapp/89998654777/link.com"
             },
             {
@@ -36,7 +36,7 @@ export default {
                 "code": "48730000",
                 "email": "matos_julia@gmail.com",
                 "password": "12345678",
-                "phoneNumber": 4998145,
+                "phoneNumber": "49981458900",
                 "whatsappLink": "whatsapp/49981458900/link.com"
             }
         ])
