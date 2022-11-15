54d47d65e505be50edb12176d3350eca6ef63bbc
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-15 17:04:50 -0300

    constants created

---
+++ src/infrastructure/config/constants.config.ts
@@ -0,0 +1,21 @@
+export default {
+    CLIENTS: {
+        MESSAGES: {
+            ERROR: {
+                USER_NOT_EXIST: `Usuario {USER_ID} não encontrado`,
+            }
+        }
+    },
+    AUTH: {
+        MESSAGES: {
+            ERROR: {
+                INVALID_EMAIL: `Senha ou email inválido, tente novamente`,
+                INVALID_PASS: `Senha ou email inválido, tente novamente`,
+                TOKEN_REQUIRED: `Para acessar os dados, o token é necessário!`,
+                VERIFY_AUTH: `Por favor, verifique a autenticação!`,
+                INVALID_TOKEN: `Token inválido...`,
+                INVALID_DATA: `Dados inválidos!!`
+            }
+        }
+    }
+}
