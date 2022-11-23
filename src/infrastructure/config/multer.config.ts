ebbe95bb0a2074ddb8317b439ef4de1b9828c861
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-23 13:38:48 -0300

    add multer-s3

--- src/infrastructure/config/multer.config.ts
+++ src/infrastructure/config/multer.config.ts
@@ -1,12 +1,15 @@
 import { randomBytes } from "crypto";
-import { diskStorage, Options } from "multer";
-import { resolve } from "path";
+import multer, { Options } from "multer";
+import path, { resolve } from "path";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
+const s3 = new S3Client({})
 
-
-export const multerConfig = {
-    dest: resolve(__dirname, '..', '..', '..', 'uploads'),
-    storage: diskStorage({
+export const storageTypes = {
+   
+       dest: path.resolve(__dirname, '..', '..', '..', 'uploads'),
+       local: multer.diskStorage({
         destination:(req, file, callback) => {
             callback(null, resolve(__dirname, '..', '..', '..', 'uploads'))
         },
@@ -19,9 +22,22 @@ export const multerConfig = {
                 callback(null, filename)
             })
         }
-    }),
+    }),/*
+    s3: multerS3({
+        s3: s3,
+        bucket: String(process.env.BUCKET_NAME),
+        key: (req, file, callback) => {
+            randomBytes(16, (error, hash) =>{
+                if(error) {
+                    callback(error, file.filename)
+                }
+                const filename = `${hash.toString('hex')}.png`
+                callback(null, filename)
+            })
+        }
+    }),*/
     limits:{
-        fileSize: 4 * 1024 * 1024 //4mb
+        fileSize: 2 * 1024 * 1024 //2mb
     },
     fileFilter: (req, file, callback) => {
         const formats = [
@@ -29,11 +45,13 @@ export const multerConfig = {
             'image/jpg',
             'image/png',
         ];
-
+    
         if (formats.includes(file.mimetype)) {
             callback(null,true)
         } else {
             callback (new Error ('format not accepted'))
         }
     }
-} as Options;
+   
+} as Options
+
