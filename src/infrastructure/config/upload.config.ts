c1f955c640b38f378f60c7a8e4e3eb49636eabd2
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-28 09:57:51 -0300

    review upload

--- src/infrastructure/config/upload.config.ts
+++ src/infrastructure/config/upload.config.ts
@@ -3,7 +3,8 @@ import "dotenv/config";
 const uploadConfig = {
     region: process.env.AWS_DEFAULT_REGION, 
     accessKeyId: String(process.env.AWS_ACCESS_KEY_ID), 
-    secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY)
+    secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
+    bucketName: String(process.env.BUCKET_NAME)
 }
 
 export default uploadConfig
