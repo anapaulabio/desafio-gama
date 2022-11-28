79c120e5aa3477da3c189114571a6e916e1ad749
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-11-28 09:58:38 -0300

    review caracteres migrations

--- src/infrastructure/persistence/mysql/migrations/1-users.migrations.ts
+++ src/infrastructure/persistence/mysql/migrations/1-users.migrations.ts
@@ -9,13 +9,13 @@ export default {
                 autoIncrement: true
             },
             name: Sequelize.DataTypes.STRING,
-            code: Sequelize.DataTypes.STRING,
+            code: Sequelize.DataTypes.STRING(10),
             email: {
                 type: Sequelize.DataTypes.STRING,
                 unique: true,
             },
             password: Sequelize.DataTypes.STRING,
-            phoneNumber: Sequelize.DataTypes.STRING,
+            phoneNumber: Sequelize.DataTypes.STRING(12),
             createdAt: Sequelize.DataTypes.DATE,
             updatedAt:  Sequelize.DataTypes.DATE
         })
--- src/infrastructure/persistence/mysql/migrations/2-vets.migration.ts
+++ src/infrastructure/persistence/mysql/migrations/2-vets.migration.ts
@@ -21,11 +21,11 @@ export default {
             avatar: Sequelize.DataTypes.STRING,
             specialty: Sequelize.DataTypes.STRING,
             crmv: Sequelize.DataTypes.STRING,
-            aboutMe: Sequelize.DataTypes.STRING,
+            aboutMe: Sequelize.DataTypes.STRING(300),
             queryValue: Sequelize.DataTypes.INTEGER,
             queryDutyValue: Sequelize.DataTypes.INTEGER,
             formation: Sequelize.DataTypes.STRING,
-            experience: Sequelize.DataTypes.STRING,
+            experience: Sequelize.DataTypes.STRING(300),
             teleconsultation: Sequelize.DataTypes.BOOLEAN,
             createdAt: Sequelize.DataTypes.DATE,
             updatedAt:  Sequelize.DataTypes.DATE
